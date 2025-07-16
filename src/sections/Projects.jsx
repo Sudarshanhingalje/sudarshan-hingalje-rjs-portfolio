555b9130fab0f91c9551868b342a2198586b8582
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-17 00:05:00 +0530

    🔄 Auto commit

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -14,7 +14,7 @@ const Projects = () => {
           My Projects
         </motion.h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 bg">
           {projectsDone.map((project, index) => (
             <motion.div
               key={index}
