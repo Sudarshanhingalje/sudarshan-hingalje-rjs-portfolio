be617f0f80c5a96cfb1cadc699592f5361e23d2f
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-17 02:05:00 +0530

    🔄 Auto commit

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -52,6 +52,16 @@ export default function Header() {
             Developer
           </span>
         </p>
+        <svg width="100" height="100">
+          <circle
+            cx="50"
+            cy="50"
+            r="40"
+            stroke="blue"
+            stroke-width="4"
+            fill="none"
+          />
+        </svg>
 
         <a
           href="#about"
