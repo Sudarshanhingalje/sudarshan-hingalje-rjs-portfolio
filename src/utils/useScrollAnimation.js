a28d052c67be0dd80885f51f5f29a651e4ff20cb
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-16 23:46:00 +0530

    🔄 Auto commit

--- src/utils/useScrollAnimation.js
+++ src/utils/useScrollAnimation.js
@@ -22,7 +22,7 @@ export default function useScrollAnimation() {
     });
 
     // ✅ Pin About Section with parallax
-    gsap.to(".about-avatar", {
+    gsap.to("#about .about-avatar", {
       opacity: 0,
       scrollTrigger: {
         trigger: "#skills", // fade out as Skills section starts
