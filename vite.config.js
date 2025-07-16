4654fa8b674ed957a2a5d6d838dceacbf50623bb
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-16 22:19:02 +0530

    🔄 Auto commit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [react()],
