27d9ea24a6007996465046f31d7fa3520f292cc1
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-18 03:46:02 +0530

    🔄 Auto commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { lazy, Suspense, useEffect, useState } from "react";
+import { Toaster } from "react-hot-toast";
 import Loader from "./components/Loader";
 import Main from "./components/Main";
 import Wheel from "./components/Wheel";
