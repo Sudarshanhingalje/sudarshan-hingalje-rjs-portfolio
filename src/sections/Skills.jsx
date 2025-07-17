cb56722229060a6bf5cf7451a2a635051ea7a723
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-18 01:44:00 +0530

    🔄 Auto commit

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import skillsLerned from "../data/skills/SkillsLerned";
+import { skillsLerned } from "../data/skills/SkillsLerned";
 
 const Skills = () => {
   return (
