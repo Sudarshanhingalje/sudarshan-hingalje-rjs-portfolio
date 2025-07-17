7bff2372292162b81b0a9dd8e0c710238d092dd0
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-17 16:44:00 +0530

    🔄 Auto commit

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,13 @@
-import loaderGif from "../assets/Loader.gif";
+import loaderGif from "../assets/loader.gif"; // Adjust path as needed
 
 const Loader = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-      <img src={loaderGif} alt="Loading..." className="w-75 h-75" />
+      <img
+        src={loaderGif}
+        alt="Loading..."
+        className="w-20 h-20 object-contain"
+      />
     </div>
   );
 };
