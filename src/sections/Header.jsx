2e91ec8a3b389befaee10d3d404dd54250d71310
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-17 22:18:00 +0530

    🔄 Auto commit

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -65,7 +65,7 @@ export default function Header() {
           </a>
 
           <a
-            href=
+            href={resumeUrl}
             download
             className="flex items-center justify-center gap-2 bg-white hover:bg-gray-200 text-black font-medium px-6 py-3 rounded-full text-lg sm:text-xl transition duration-300 shadow-md"
           >
