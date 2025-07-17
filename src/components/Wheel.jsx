32761d3a237bf60ee524996aa1e138005f1c0f50
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-17 10:23:00 +0530

    🔄 Auto commit

--- src/components/Wheel.jsx
+++ src/components/Wheel.jsx
@@ -1,18 +1,17 @@
 import { motion, useScroll, useTransform } from "framer-motion";
 import wheelImg from "../assets/wheel.png";
+
 const ScrollWheel = () => {
   const { scrollY } = useScroll();
   const rotate = useTransform(scrollY, [0, 1000], [0, 360]);
 
   return (
-    <div className="flex justify-center items-center  min-h-[200vh] bg-black">
-      <motion.img
-        src={wheelImg}
-        alt="Steering Wheel"
-        style={{ rotate }}
-        className="w-40 h-40 pointer-events-none select-none"
-      />
-    </div>
+    <motion.img
+      src={wheelImg}
+      alt="Steering Wheel"
+      style={{ rotate }}
+      className="fixed right-10 top-1/2 -translate-y-1/2 w-40 h-40 z-50 pointer-events-none select-none"
+    />
   );
 };
 
