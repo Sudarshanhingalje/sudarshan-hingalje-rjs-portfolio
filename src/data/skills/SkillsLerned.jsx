037ac3581febeef3e6a725e32dcdc5554ac118e1
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-18 01:48:01 +0530

    🔄 Auto commit

--- src/data/skills/SkillsLerned.jsx
+++ src/data/skills/SkillsLerned.jsx
@@ -2,24 +2,38 @@ export const skillsLerned = [
   {
     category: "Frontend",
     items: [
-      { name: "HTML", image: "html.svg" },
-      { name: "CSS", image: "css.svg" },
-      { name: "JavaScript", image: "javascript.svg" },
-      { name: "React", image: "react.svg" },
+      { name: "HTML", image: "/skill/html.svg" },
+      { name: "CSS", image: "/skill/css.svg" },
+      { name: "JavaScript", image: "/skill/javascript.svg" },
+      { name: "React", image: "/skill/react.svg" },
+      { name: "Tailwind", image: "/skill/tailwind.svg" },
+      { name: "Bootstrap", image: "/skill/bootstrap.svg" },
     ],
   },
   {
     category: "Backend",
     items: [
-      { name: "Node.js", image: "nodejs.svg" },
-      { name: "Express.js", image: "expressjs.svg" },
+      { name: "Node.js", image: "/skill/nodejs.svg" },
+      { name: "Express.js", image: "/skill/express.svg" },
+      { name: "Java", image: "/skill/java.svg" },
+      { name: "Spring Boot", image: "/skill/springboot.svg" },
     ],
   },
   {
     category: "Database",
     items: [
-      { name: "MongoDB", image: "mongodb.svg" },
-      { name: "MySQL", image: "mysql.svg" },
+      { name: "MongoDB", image: "/skill/mongodb.svg" },
+      { name: "MySQL", image: "/skill/mysql.svg" },
+    ],
+  },
+  {
+    category: "Tools & Platforms",
+    items: [
+      { name: "Git", image: "/skill/git.svg" },
+      { name: "GitHub", image: "/skill/github.svg" },
+      { name: "Docker", image: "/skill/docker.svg" },
+      { name: "Jenkins", image: "/skill/jenkins.svg" },
+      { name: "VS Code", image: "/skill/vscode.svg" },
     ],
   },
 ];
