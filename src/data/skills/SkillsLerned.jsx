2edf8334330da51129644b31b97cca8419261a85
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-18 01:32:00 +0530

    🔄 Auto commit

--- src/data/skills/SkillsLerned.jsx
+++ src/data/skills/SkillsLerned.jsx
@@ -6,14 +6,34 @@ export const skillsLerned = [
       { name: "CSS", image: "/skill/css.svg" },
       { name: "JavaScript", image: "/skill/javascript.svg" },
       { name: "React", image: "/skill/react.svg" },
+      { name: "Tailwind", image: "/skill/tailwind.svg" },
+      { name: "Bootstrap", image: "/skill/bootstrap.svg" },
     ],
   },
   {
     category: "Backend",
     items: [
       { name: "Node.js", image: "/skill/nodejs.svg" },
-      { name: "Express", image: "/skill/express.svg" },
+      { name: "Express.js", image: "/skill/express.svg" },
+      { name: "Java", image: "/skill/java.svg" },
+      { name: "Spring Boot", image: "/skill/springboot.svg" },
+    ],
+  },
+  {
+    category: "Database",
+    items: [
       { name: "MongoDB", image: "/skill/mongodb.svg" },
+      { name: "MySQL", image: "/skill/mysql.svg" },
+    ],
+  },
+  {
+    category: "Tools & Platforms",
+    items: [
+      { name: "Git", image: "/skill/git.svg" },
+      { name: "GitHub", image: "/skill/github.svg" },
+      { name: "Docker", image: "/skill/docker.svg" },
+      { name: "Jenkins", image: "/skill/jenkins.svg" },
+      { name: "VS Code", image: "/skill/vscode.svg" },
     ],
   },
 ];
