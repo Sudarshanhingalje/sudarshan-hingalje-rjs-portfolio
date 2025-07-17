71815023e71c45aefb487dd6f0b2c0ac0faaa576
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-17 14:47:00 +0530

    🔄 Auto commit

--- src/components/Wheel.jsx
+++ src/components/Wheel.jsx
@@ -1,7 +1,7 @@
 import { animate, motion, useMotionValue } from "framer-motion";
 import { useCallback, useEffect, useRef, useState } from "react";
 import clickSoundFile from "../assets/click.mp3";
-import wheelImg from "../assets/wheel-fixed.png";
+import wheelImg from "../assets/wheel-fixed.svg";
 
 const sections = [
   "header",
