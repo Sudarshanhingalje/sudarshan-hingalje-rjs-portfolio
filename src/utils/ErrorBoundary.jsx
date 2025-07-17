4b4f8c75ae742426bed1c265f6b1ac052b609e7c
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-17 19:28:00 +0530

    🔄 Auto commit

---
+++ src/utils/ErrorBoundary.jsx
@@ -0,0 +1,20 @@
+// ErrorBoundary.jsx
+import { Component } from "react";
+
+export class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h2>Something went wrong while loading icons.</h2>;
+    }
+    return this.props.children;
+  }
+}
