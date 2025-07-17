66c70bdbf7f7b242af53f20afbc649fab990a73b
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-17 19:31:00 +0530

    🔄 Auto commit

--- src/utils/ErrorBoundary.jsx
+++ src/utils/ErrorBoundary.jsx
@@ -1,7 +1,7 @@
 // ErrorBoundary.jsx
 import { Component } from "react";
 
-export class ErrorBoundary extends Component {
+class ErrorBoundary extends Component {
   constructor(props) {
     super(props);
     this.state = { hasError: false };
@@ -18,3 +18,5 @@ export class ErrorBoundary extends Component {
     return this.props.children;
   }
 }
+
+export default ErrorBoundary;
