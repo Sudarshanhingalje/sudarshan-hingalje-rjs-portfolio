1392f1f0395d6ebfec2e238dc87025d54438f742
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-18 08:08:00 +0530

    🔄 Auto commit

--- src/components/TalkingAvatar.jsx
+++ src/components/TalkingAvatar.jsx
@@ -1,9 +1,30 @@
+// ✅ TalkingAvatar.jsx
+import { useEffect, useState } from "react";
 import avatarImg from "../assets/yoga.svg";
 import "../styles/index.css";
 
-const TalkingAvatar = ({ isSpeaking }) => {
+const TalkingAvatar = ({ isSpeaking, message }) => {
+  const [currentMessage, setCurrentMessage] = useState("");
+
+  useEffect(() => {
+    if (isSpeaking && message) {
+      setCurrentMessage(message);
+    } else {
+      setCurrentMessage("");
+    }
+  }, [isSpeaking, message]);
+
   return (
     <div className="relative flex flex-col items-center mt-10">
+      {/* Speech bubble */}
+      {currentMessage && (
+        <div className="absolute -top-10 left-1/2 transform -translate-x-1/2 bg-white border border-gray-300 rounded-lg p-3 text-sm shadow-lg w-64 text-gray-800">
+          {currentMessage}
+          <div className="absolute -bottom-2 left-1/2 transform -translate-x-1/2 w-3 h-3 bg-white rotate-45 border-l border-b border-gray-300"></div>
+        </div>
+      )}
+
+      {/* Avatar */}
       <img
         src={avatarImg}
         alt="avatar"
