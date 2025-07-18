3ba02d6ca652474e349c1502f2789b00f289abab
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-18 17:08:00 +0530

    🔄 Auto commit

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -68,7 +68,7 @@ export default function Header() {
   return (
     <section
       id="header"
-      className="relative h-screen w-full text-white overflow-hidden bg-[#fffffff4]"
+      className="relative h-screen w-full font-cinzel text-white overflow-hidden bg-[#fffffff4]"
     >
       <div className="absolute inset-0 z-0 bg-grid-pattern" />
 
