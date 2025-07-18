48ae09230bff6856b68c092d4102fac8a1e076a3
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-18 10:43:01 +0530

    🔄 Auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,6 @@ export default function Navbar() {
   return (
     <div>
       <motion.nav className="fixed top-0 left-0 w-full z-50 px-4 sm:px-6 md:px-10 py-3 flex  justify-between bg-transparent">
-        <div className="text-white text-base sm:text-lg md:text-xl font-bold tracking-wide font-cinzel underline"></div>
-
         {/* Logo on the right */}
         <button
           onClick={() => window.location.reload()}
