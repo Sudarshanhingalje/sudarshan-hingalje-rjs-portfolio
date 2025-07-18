ae2ee00614cdc634c9207bbd0f49879449a4d6ef
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-18 17:20:00 +0530

    🔄 Auto commit

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
   }, []);
 
   return (
-    <Main>
+    <Main className="dark">
       <Toaster position="top-right" reverseOrder={false} />
       <div className="fixed top-4 right-4 mt-4 ml-auto mr-4 z-50">
         <MusicToggleButton />
