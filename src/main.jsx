b45ac181b3df88377bdec1349ba9ee64c116c958
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-19 15:07:00 +0530

    🔄 Auto commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx";
 import "./styles/curser.css";
 import "./styles/global.css";
 import "./styles/index.css";
-import "./styles/ScrollSnapping.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
