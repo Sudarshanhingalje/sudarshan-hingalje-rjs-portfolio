3e0fa0a6d895e84414a03ada53a3f7a8450aca36
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-19 12:58:00 +0530

    🔄 Auto commit

--- src/data/projects/ProjectDone.jsx
+++ src/data/projects/ProjectDone.jsx
@@ -4,31 +4,35 @@ export const projectsDone = [
     title: "The Wild Oasis (Cabin Booking App)",
     description:
       "A full‑stack hotel booking platform with both admin and customer interfaces. Built using React, Supabase, styled‑components, React Query.",
-    img: "src/assets/project/thewildoasiswebsite.png", 
-    link:
+    img: "src/assets/project/thewildoasiswebsite.png",
+    link: "https://the-wild-oasis-website-cabins.vercel.app/",
   },
   {
     title: "The Wild Oasis (Admin Dashboard)",
     description:
       "Admin panel for managing cabins, bookings, and users with interactive charts.",
     img: "src/assets/project/thewildoasis.png",
+    link: "https://the-wild-oasis-website-cabins.vercel.app/",
   },
   {
     title: "Fast React Pizza",
     description:
       "Modern pizza ordering app built with React, Redux Toolkit and Tailwind CSS.",
     img: "src/assets/project/fastreactpizza.png",
+    link: "https://the-wild-oasis-website-cabins.vercel.app/",
   },
   {
     title: "ScrapSavvy Waste Platform",
     description:
       "Collaborative scrap collection platform. Built with Firebase backend.",
     img: "src/assets/project/scrapsavvy.png",
+    link: "https://the-wild-oasis-website-cabins.vercel.app/",
   },
   {
     title: "Pizza Delivery App",
     description:
       "Feature‑rich pizza ordering system with Razorpay payment, MongoDB, Axios, Redux.",
     img: "src/assets/project/pizzastore.png",
+    link: "https://the-wild-oasis-website-cabins.vercel.app/",
   },
 ];
