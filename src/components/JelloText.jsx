3d1f0fdf83bfa1e4bf39c4f3a201a381b544a3df
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-19 18:30:00 +0530

    🔄 Auto commit

--- src/components/JelloText.jsx
+++ src/components/JelloText.jsx
@@ -131,7 +131,7 @@ const JelloText = () => {
         ref={txtRef}
         className="text-[5vw] font-[200] tracking-[-1vw] leading-[0.6] text-white select-none shadow-jello"
       >
-        {"sudarshan hingalje PORTFOLIO".split(" ").map((char, i) => (
+        {"sudarshan hingalje PORTFOLIO".split("").map((char, i) => (
           <span
             key={i}
             className="inline-block cursor-pointer pt-[1.08vw]"
