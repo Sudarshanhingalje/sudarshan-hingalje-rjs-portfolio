14080634c174c39cf1178c3d8e48c07086689349
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-19 14:53:00 +0530

    🔄 Auto commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./styles/curser.css";
 import "./styles/global.css";
 import "./styles/index.css";
+import "./styles/ScrollSnapping.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- src/utils/SmoothScroll.jsx
+++ src/utils/SmoothScroll.jsx
@@ -1,7 +1,6 @@
 import Lenis from "@studio-freight/lenis";
 import { useEffect } from "react";
-import { motion } from "framer-motion";
-import "./SmoothScroll.css";
+import "./styles/SmoothScroll.css";
 
 export default function SmoothScroll({ children }) {
   useEffect(() => {
