4174753231a5048a82144f4a71eae1461724ecae
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-19 15:20:01 +0530

    🔄 Auto commit

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -3,68 +3,72 @@ import bgImage from "../assets/brand2.svg";
 
 const Footer = () => {
   return (
-    id="footer"
-    <footer className="flex flex-col items-center pb-10 text-white ">
-      <p className="text-xs text-gray-400 py-4"> Made with ❤️ React Js+Vite</p>
-      Sudarshan Hingalje © 2025
-      <div className="my-4">
-        <button
-          onClick={() =>
-            window.scrollTo({
-              top: 0,
-              behavior: "smooth",
-            })
-          }
-          className="focus:outline-none"
-        >
-          <img
-            src={bgImage}
-            alt="Logo"
-            className="h-20 md:h-20 w-full object-contain cursor-pointer"
-          />
-        </button>
-      </div>
-      <ul className="flex items-center justify-center gap-6 text-xl">
-        <li>
-          <a
-            href="https://www.linkedin.com/in/sudarshanhingalje"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-white"
+    <section id="footer">
+      <footer className="flex flex-col items-center pb-10 text-white ">
+        <p className="text-xs text-gray-400 py-4">
+          {" "}
+          Made with ❤️ React Js+Vite
+        </p>
+        Sudarshan Hingalje © 2025
+        <div className="my-4">
+          <button
+            onClick={() =>
+              window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+              })
+            }
+            className="focus:outline-none"
           >
-            <FaLinkedin />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://twitter.com/sudarshanhingalje"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-green-500"
-          >
-            <FaTwitter />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.instagram.com/sudarshanhingalje"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-white"
-          >
-            <FaInstagram />
-          </a>
-        </li>
-        <li>
-          <a
-            href="mailto:sudarshanhingalje1@gmail.com"
-            className="hover:text-white"
-          >
-            <FaEnvelope />
-          </a>
-        </li>
-      </ul>
-    </footer>
+            <img
+              src={bgImage}
+              alt="Logo"
+              className="h-20 md:h-20 w-full object-contain cursor-pointer"
+            />
+          </button>
+        </div>
+        <ul className="flex items-center justify-center gap-6 text-xl">
+          <li>
+            <a
+              href="https://www.linkedin.com/in/sudarshanhingalje"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-white"
+            >
+              <FaLinkedin />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/sudarshanhingalje"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-green-500"
+            >
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/sudarshanhingalje"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-white"
+            >
+              <FaInstagram />
+            </a>
+          </li>
+          <li>
+            <a
+              href="mailto:sudarshanhingalje1@gmail.com"
+              className="hover:text-white"
+            >
+              <FaEnvelope />
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </section>
   );
 };
 
