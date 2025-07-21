3ff29f4bebeccf0b8756c5a3bbdfa7ddc0d5f148
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-07-21 23:28:00 +0530

    🔄 Auto commit

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -4,10 +4,7 @@ import bgImage from "../assets/brand2.svg";
 
 const Footer = () => {
   return (
-    <footer
-      id="footer"
-      className="flex flex-col items-center pb-10 text-white "
-    >
+    <footer className="flex flex-col items-center pb-10 text-white ">
       <p className="text-xs text-gray-400 py-4"> Made with ❤️ React Js+Vite</p>
       Sudarshan Hingalje © 2025
       <div className="my-4">
